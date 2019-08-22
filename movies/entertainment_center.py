f083b936b95191f665201c72075a00402e991911
Brian Futrell <bfutrel@gmail.com>
2019-08-22 15:28:36 -0400

    more movie instances added

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -20,5 +20,25 @@ walle = media.Movie("WALL-E",
 						"https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/WALL-Eposter.jpg/220px-WALL-Eposter.jpg", 
 						"https://www.youtube.com/watch?v=ZisWjdjs-gM")
 
-print(walle.storyline)
-walle.show_trailer()
+#print(walle.storyline)
+#walle.show_trailer()
+
+school_of_rock = media.Movie("School of Rock", 
+						"Using rock music to learn", 
+						"https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", 
+						"https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", 
+						"A rat is a chef in Paris", 
+						"https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", 
+						"https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight In Paris", 
+						"Going back in time to meet authors", 
+						"https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg", 
+						"https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games", 
+						"A really real reality show", 
+						"https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg", 
+						"https://www.youtube.com/watch?v=PbA63a7H0bo")
